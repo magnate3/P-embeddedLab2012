45f444aad1a5a7eee1527e1accab830b1283b852
Jim Huang <jserv@0xlab.org>
2012-10-23 10:52:09 +0800

    [Lab-5] coverage: Improve the comments

--- Lab-5/coverage/hello.c
+++ Lab-5/coverage/hello.c
@@ -1,13 +1,13 @@
 /* dumping gcov data at runtime - note: no error handling to keep it simple */
 /* Compile: gcc -Wall -fprofile-arcs -ftest-coverage hello.c -o hello
  * Run: ./hello &
- *
- kill -s SIGUSR1 ‘pidof hello‘
+ *      kill -s SIGUSR1 ‘pidof hello‘
  *
  * Author: Der Herr Hofr.at
  * Copyright: OpenTech EDV Research GmbH 2011
  * License: GPL V2
  */
+
 #include <stdio.h>
 #include <signal.h>
 #include <sys/types.h>
@@ -15,7 +15,8 @@
 #include <stdlib.h>
 
 static unsigned long long i = 0;
-void __gcov_flush(void); /* check in gcc sources gcc/gcov-io.h for the prototype */
+void __gcov_flush(void); /* check in gcc sources gcc/gcov-io.h for 
+			    the prototype */
 
 void my_handler(int signum)
 {
@@ -28,14 +29,19 @@ int main(int argc, char **argv)
 {
 	struct sigaction new_action, old_action;
 	int n;
+
 	/* setup signal hander */
 	new_action.sa_handler = my_handler;
 	sigemptyset(&new_action.sa_mask);
 	new_action.sa_flags = 0;
 	sigaction(SIGUSR1, NULL, &old_action);
+
 	if (old_action.sa_handler != SIG_IGN)
 		sigaction (SIGUSR1, &new_action, NULL);
-	/* infinite loop - to exemplify dumping coverage data while program runs */
+
+	/* infinite loop - to exemplify dumping coverage data while
+	 * program runs
+	 */
 	for (n = 0; ; n++)
 		i++;
 }
