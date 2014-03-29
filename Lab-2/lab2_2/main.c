7a4623e37abde09f43936ee635679d9d7d00c8a2
MengJia Tsai <mctsai@gmail.com>
2014-03-29 21:30:44 +0800

    port macro to arm assembly

--- Lab-2/lab2_2/main.c
+++ Lab-2/lab2_2/main.c
@@ -1,68 +1,105 @@
 #define USE_STDPERIPH_DRIVER
 #include "stm32f10x.h"
 
+
+//#include <stdio.h>
+#include <setjmp.h>
+//#include <stdlib.h>
+
+#define STACK_SIZE 4096
+
+/*
+ * Change SP prior to calling setjmp so that longjmp will
+ * start the routine with 'stackptr'.
+ */
+#define SAVE_STACK_POINTER_ASM(savedstack, stackptr) \
+	"mov sp, %[savedstack]\n" /* savedstack <- SP */ \
+	"mov %[stackptr], sp"    /* SP <- stackptr */
+
+#define SAVE_STACK_POINTER(savedstack, stackptr) \
+do { \
+	asm volatile ( SAVE_STACK_POINTER_ASM(savedstack, stackptr) \
+	: [savedstack] "=r" (savedstack): [stackptr] "r" (stackptr) ); \
+} while (0)
+
+/* Restore "normal" stack prior to return */
+#define RESTORE_STACK_ASM(savedstack) \
+	"mov %[savedstack], sp"
+
+#define RESTORE_STACK(savedstack) \
+do { \
+	asm volatile ( RESTORE_STACK_ASM(savedstack) \
+	: : [savedstack] "r" (savedstack)); \
+} while (0)	
+
+jmp_buf routine1_buf;
+jmp_buf routine2_buf;
+
+void routine1()
+{
+	volatile unsigned count = 0;
+
+	/* Start of Routine #1 */
+	setjmp(routine1_buf);
+
+//	printf("[Routine 1] pass %d\n", ++count);
+
+	longjmp(routine2_buf, 1);
+}
+
+void create_routine1(const void *stackptr)
+{
+	register unsigned long savedstack;
+
+	SAVE_STACK_POINTER(savedstack, stackptr);
+
+	if (setjmp(routine1_buf) == 0) {
+		RESTORE_STACK(savedstack);
+	}
+	else {
+		/* We got here through longjmp */
+		routine1();
+	}
+}
+
+#define FINAL_NUM 5
+void routine2()
+{
+	volatile unsigned count = 0;
+
+	/* Start of Routine #2 */
+	setjmp(routine2_buf);
+
+//	printf("[Routine 2] pass %d\n", (FINAL_NUM - ++count));
+
+	if (count < FINAL_NUM)
+		longjmp(routine1_buf, 1);
+
+//	exit(0);
+}
+
+void create_routine2(const void *stackptr)
+{
+	register unsigned long savedstack;
+
+	SAVE_STACK_POINTER(savedstack, stackptr);
+
+	if (setjmp(routine2_buf) == 0) {
+		RESTORE_STACK(savedstack);
+	}
+	else {
+		routine2();
+	}
+}
+
 int main(void)
 {
-
-// init_rs232 in stm32_p103.c
-    USART_InitTypeDef USART_InitStructure;
-    GPIO_InitTypeDef GPIO_InitStructure;
-//
-//	// should successful to connect to remote
-//    /* Enable peripheral clocks. */
-//    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
-//    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-//	// stm32f10x_rcc.c
-//	void RCC_APB2PeriphClockCmd(uint32_t RCC_APB2Periph, FunctionalState NewState)
-//	{
-//	  /* Check the parameters */
-//	  assert_param(IS_RCC_APB2_PERIPH(RCC_APB2Periph));			// use call
-//	  assert_param(IS_FUNCTIONAL_STATE(NewState));
-//	  if (NewState != DISABLE)
-//	  {
-//	    RCC->APB2ENR |= RCC_APB2Periph;
-//	  }
-//	  else
-//	  {
-//	    RCC->APB2ENR &= ~RCC_APB2Periph;
-//	  }
-//	}
-
-//    /* Configure USART2 Rx pin as floating input. */
-//    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-//    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-//    GPIO_Init(GPIOA, &GPIO_InitStructure);
-//
-//    /* Configure USART2 Tx as alternate function push-pull. */
-//    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-//    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-//    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-//    GPIO_Init(GPIOA, &GPIO_InitStructure);
-//
-//    /* Configure the USART2 */
-//    USART_InitStructure.USART_BaudRate = 9600;
-//    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-//    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-//    USART_InitStructure.USART_Parity = USART_Parity_No;
-//    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-//    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-//    USART_Init(USART2, &USART_InitStructure);
-//    USART_Cmd(USART2, ENABLE);
-
-
-//enable rs232 in stm32_p103.c
-    /* Enable the RS232 port. */
-//    USART_Cmd(USART2, ENABLE);
-
-
-//	USART_SendData(USART2, 'H');
-//	USART_SendData(USART2, 'e');
-//	USART_SendData(USART2, 'l');
-//	USART_SendData(USART2, 'l');
-//	USART_SendData(USART2, 'o');
-//	USART_SendData(USART2, '!');
-//	USART_SendData(USART2, '\n');
-
+//	create_routine1((char *) malloc(STACK_SIZE) + STACK_SIZE);
+//	create_routine2((char *) malloc(STACK_SIZE) + STACK_SIZE);
+	longjmp(routine1_buf, 1);
 
 	return 0;
 }
+
+
+
